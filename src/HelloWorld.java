c2b2f0ba173dd77af77f5b32e00b7aa5124538af
Jhosep Guadalupe <jhosep.guadalupe>
2021-06-14 22:03:59 -0500

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,40 @@
+//MI CLASE DE JAVA - PRIMERA CLASE
+
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        int myVariable = 10;
+
+        System.out.println(myVariable);
+
+        myVariable = 5;
+
+        System.out.println(myVariable);
+
+        String variableCadena = "Saludos";
+
+        System.out.println(variableCadena);
+
+        variableCadena = "Adíos";
+
+        System.out.println(variableCadena);
+
+        //var - inferencia de tipo
+        var miVariableEntera2 = 15;
+
+        System.out.println(miVariableEntera2);
+        
+        
+        // VAlores permitidos en el nombre de variables
+        var miVariable = "";
+        var _miVariable ="";
+        var $miVariable = "";
+        // var áVariable = ""; no es recomendable utulizar tildes
+        // #variable = ""; no se puede utilizar caracteres especiales
+        
+        
+        
+        
+
+    }
+}
