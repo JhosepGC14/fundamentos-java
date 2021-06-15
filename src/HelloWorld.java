ee6360a0069c7718164afbf701f9b4030b163f74
Jhosep Guadalupe <jhosep.guadalupe>
2021-06-14 22:42:39 -0500

    class Scanner

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,40 +1,58 @@
-//MI CLASE DE JAVA - PRIMERA CLASE
 
+import java.util.Scanner;
+
+//MI CLASE DE JAVA - PRIMERA CLASE
 public class HelloWorld {
 
     public static void main(String args[]) {
-        int myVariable = 10;
 
-        System.out.println(myVariable);
+        String usuario = "Jhosep";
 
-        myVariable = 5;
+        String titulo = "Ingeniero";
 
-        System.out.println(myVariable);
+        String union = usuario + " " + titulo;
 
-        String variableCadena = "Saludos";
+        System.out.println("union = " + union);
 
-        System.out.println(variableCadena);
+        //<----------------------->
+        var i = 2;
+        var j = 4;
 
-        variableCadena = "Adíos";
+        System.out.println(i + j);
+        System.out.println(i + j + usuario);
 
-        System.out.println(variableCadena);
+        //CONTEXTO CADENA - si la variable a imprimir comienza con una cadena 
+        // entonces lo demas tambien lo toma como cadena por entre concatena.
+        System.out.println(usuario + i + j);
 
-        //var - inferencia de tipo
-        var miVariableEntera2 = 15;
+        // Cuando se le agrega el parentesis , este modifica la priodidad por 
+        // entre suma primero y luego hace el contexto cadena.
+        System.out.println(usuario + (i + j));
 
-        System.out.println(miVariableEntera2);
-        
-        
-        // VAlores permitidos en el nombre de variables
-        var miVariable = "";
-        var _miVariable ="";
-        var $miVariable = "";
-        // var áVariable = ""; no es recomendable utulizar tildes
-        // #variable = ""; no se puede utilizar caracteres especiales
-        
-        
-        
-        
+        //<---------------------------------------------->
+        // CARACTERES ESPECIALES EN JAVA
+        String nombre = "Jhosep Adolfo";
+
+        //salto de linea
+        System.out.println("nombre: \n" + nombre);
+        //tabuladores
+        System.out.println("nombre: \t " + nombre);
+        //retroceso
+        System.out.println("nombre = \b\b" + nombre);
+        //comilla simple
+        System.out.println("nombre = '" + nombre + "'");
+        //comilla doble
+        System.out.println("nombre = \"" + nombre + "\"");
+
+        //CLASE SCANNER PARA PEDIR DATOS DE INGRESO
+        System.out.println("Escribir tu nombre : ");
+        Scanner consola = new Scanner(System.in);
+        String name = consola.nextLine();
+        System.out.println("usuario = " + name);
+
+        System.out.println("Escribir tu profesion : ");
+        String profesion = consola.nextLine();
+        System.out.println("profesion = " + profesion);
 
     }
 }
