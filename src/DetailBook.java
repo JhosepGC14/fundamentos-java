af316b3f4e369c3e2f422e18d24f32c36c11d7af
Jhosep Guadalupe <jhosep.guadalupe>
2021-06-14 22:51:16 -0500

    it add DetailBookClass

---
+++ src/DetailBook.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jhosep_PC
+ */
+public class DetailBook {
+
+    public static void main(String args[]) {
+        
+        Scanner consola = new Scanner(System.in);
+        
+        System.out.println("Ingrese el autor : ");
+        String autor = consola.nextLine();
+        
+        System.out.println("Ingrese el titulo del libro : ");
+        String titulo = consola.nextLine();
+        
+        System.out.println(titulo + " fue escrito por " + autor);
+        
+        
+    }
+
+}
